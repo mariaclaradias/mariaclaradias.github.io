324cf42fe093db5c2baa1921cb24e0baced100b9
Maria <maria.diaas@gmail.com>
2020-09-27 11:08:04 -0300

    upload childhood

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4c9eea705e5728e4e951546ff0d29dd4.js"
+  "/precache-manifest.57accfc1fa781d71895f9efa0b0c8b8e.js"
 );
 
 self.addEventListener('message', (event) => {
